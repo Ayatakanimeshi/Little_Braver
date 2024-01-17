931d3631a8da79a5191b7678f360f15900b5268c
Ayatakanimeshi <ayatakathebattler2top@gmail.com>
2024-01-18 01:23:25 +0900

    header実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  authenticated :user do
-    root 'top#index', as: :authenticated_root
-  end
-
-  unauthenticated do
-    root 'devise/sessions#new'
-  end
+  root 'top#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
